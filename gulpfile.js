edbab88dbd0735b4c2462105444e890e8bcdb066
Azgiliat <toxa2088@gmail.com>
2019-05-05 14:28:48 +0300

    index

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -17,21 +18,44 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("realese/css"))
     .pipe(server.stream());
+});
+
+gulp.task("clean", function() {
+  return del("realese");
+});
+
+gulp.task("copy", function() {
+  return gulp.src( [
+    "source/fonts/**/*.{woff, woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html"
+  ], {
+    base: "source"
+  })
+  .pipe(gulp.dest("realese"))
+});
+
+gulp.task("build_this", gulp.series("clean", "css", "copy"));
+
+gulp.task("update", function(done) {
+  server.reload();
+  done();
 });
 
 gulp.task("server", function () {
   server.init({
-    server: "source/",
+    server: "realese/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/less/**/*.less", gulp.series("css", "update"));
+  gulp.watch("source/*.html", gulp.series("copy", "update"));
 });
 
-gulp.task("start", gulp.series("css", "server"));
+gulp.task("start", gulp.series("build_this", "server"));
