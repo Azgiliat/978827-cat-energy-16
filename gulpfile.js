f1bd29ad3487f2f563e43cc34770637864baca20
Azgiliat <toxa2088@gmail.com>
2019-05-06 23:24:21 +0300

    desktop

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,24 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var del = require("del");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("release/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("release/img"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -18,27 +36,27 @@ gulp.task("css", function () {
       autoprefixer()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("realese/css"))
+    .pipe(gulp.dest("release/css"))
     .pipe(server.stream());
 });
 
 gulp.task("clean", function() {
-  return del("realese");
+  return del("release");
 });
 
 gulp.task("copy", function() {
   return gulp.src( [
-    "source/fonts/**/*.{woff, woff2}",
+    "source/fonts/*.woff2",
     "source/img/**",
     "source/js/**",
     "source/*.html"
   ], {
     base: "source"
   })
-  .pipe(gulp.dest("realese"))
+  .pipe(gulp.dest("release"))
 });
 
-gulp.task("build_this", gulp.series("clean", "css", "copy"));
+gulp.task("build_this", gulp.series("clean", "css","webp","images" , "copy"));
 
 gulp.task("update", function(done) {
   server.reload();
@@ -47,7 +65,7 @@ gulp.task("update", function(done) {
 
 gulp.task("server", function () {
   server.init({
-    server: "realese/",
+    server: "release/",
     notify: false,
     open: true,
     cors: true,
